f577a858bcc63e8fc376aab6bb5017660ed44f6f
matt-mekha <mtmekha8@gmail.com>
2020-03-22 00:41:58 -0500

    fixed shell script

--- tools.zsh
+++ tools.zsh
@@ -4,9 +4,11 @@ option=$1
 echo [[ option == "preview" ]];
 
 if [[ $option == "deploy" ]]; then
-  jekyll build;
   cd ../houston-youth-computer-science-group.github.io;
   git pull;
+  cd ../website-source;
+  jekyll build;
+  cd ../houston-youth-computer-science-group.github.io;
   git add .;
   git commit && git push;
 elif [[ $option == "preview" ]]; then
