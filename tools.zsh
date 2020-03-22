f6911e2a57889933dd1c3938fcbbbfaa5b6ea56b
AlexDvorak <doe.jimmie@gmail.com>
2020-03-22 00:36:06 -0500

    TOOLS: - pull before changes are committed

--- tools.zsh
+++ tools.zsh
@@ -6,6 +6,7 @@ echo [[ option == "preview" ]];
 if [[ $option == "deploy" ]]; then
   jekyll build;
   cd ../houston-youth-computer-science-group.github.io;
+  git pull;
   git add .;
   git commit && git push;
 elif [[ $option == "preview" ]]; then
