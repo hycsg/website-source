eca15f0cb539e3accbd420fab666027ec3aa80b2
AlexDvorak <doe.jimmie@gmail.com>
2020-03-25 23:02:02 -0500

    more verbose deploy/preview

--- tools.zsh
+++ tools.zsh
@@ -1,16 +1,14 @@
 #!/bin/bash
 option=$1
 
-echo [[ option == "preview" ]];
 
 if [[ $option == "deploy" ]]; then
-  cd ../hycsg.github.io;
-  git pull;
-  cd ../website-source;
-  jekyll build;
-  cd ../hycsg.github.io;
-  git add .;
-  git commit && git push;
+  echo "Pulling latest rendered site";
+  cd ../hycsg.github.io && git pull;
+  echo "Building Site";
+  cd ../website-source && jekyll build;
+  echo "Committing rendered changes";
+  cd ../hycsg.github.io && git add . && git commit && git push;
 elif [[ $option == "preview" ]]; then
   echo "Preview";
   jekyll serve &&
